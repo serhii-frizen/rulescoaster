f19fac13a2ede5f9f549aebf1c7b90c9a54a0ba4
Serhii Frizen <sfrizen@gmail.com>
2022-06-22 08:59:43 -0400

    first commit

---
+++ src/main/java/com/ubs/hackathon2/rulescoaster/RulescoasterApplication.java
@@ -0,0 +1,13 @@
+package com.ubs.hackathon2.rulescoaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RulescoasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RulescoasterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ubs/hackathon2/rulescoaster/RulescoasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ubs.hackathon2.rulescoaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RulescoasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
